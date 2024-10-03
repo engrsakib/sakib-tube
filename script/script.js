8e2c24ede863f9f7fe69558a2b46385758859ad2
engrsakib <snazmussakib01@gmail.com>
2024-10-03 14:39:44 +0600

    added video

--- script/script.js
+++ script/script.js
@@ -1,32 +1,63 @@
 // theme white
-document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light');
+document.getElementsByTagName("html")[0].setAttribute("data-theme", "light");
 
 // Fetch , load and catagories
 
 // crate catagoris
-const loadCatg = () =>{
-    fetch(" https://openapi.programming-hero.com/api/phero-tube/categories")
-        .then((res) => res.json())
-        .then((data) => displatCatg(data.categories))
-         .catch((error) => console.log(error))
-}
+const loadCatg = () => {
+  fetch(" https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displatCatg(data.categories))
+    .catch((error) => console.log(error));
+};
 
 const displatCatg = (dataCtg) => {
-    const ctgCont = document.getElementById('catG');
-    dataCtg.forEach(element => {
-        console.log(element);
-        // crate button
-        const btn = document.createElement('button');
-        btn.classList = 'btn'
-        btn.innerText = element.category;
-        ctgCont.append(btn);
-    });
-}
+  const ctgCont = document.getElementById("catG");
+  dataCtg.forEach((element) => {
+    // console.log(element);
+    // crate button
+    const btn = document.createElement("button");
+    btn.classList = "btn";
+    btn.innerText = element.category;
+    ctgCont.append(btn);
+  });
+};
 
 loadCatg();
 // Videos
+const loadVedio = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displatVedio(data.videos))
+    .catch((error) => console.log(error));
+};
 
-//  https://openapi.programming-hero.com/api/phero-tube/videos
+const displatVedio = (dataVedio) => {
+  const vedioCont = document.getElementById("vedio");
+  dataVedio.forEach((element) => {
+    // crate button
+    console.log(element);
+    const card = document.createElement("div");
+    card.classList = 'card card-compact'
+    card.innerHTML = `
+    <figure>
+    <img
+      src= ${element.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>  
+    `;
+    vedioCont.append(card);
+  });
+};
+
+loadVedio();
 
 // GET: Video based on Catagory [ params ]
 
